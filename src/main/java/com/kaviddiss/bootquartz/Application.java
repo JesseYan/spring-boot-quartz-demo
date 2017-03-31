886bd2556e0015690a2edc3a73ec5252c5d2386e
JesseYan <jiayi.yan@kanche.com>
2017-03-31 11:04:07 +0800

    fork from sprint-boot-quartz-demo

--- src/main/java/com/kaviddiss/bootquartz/Application.java
+++ src/main/java/com/kaviddiss/bootquartz/Application.java
@@ -3,13 +3,18 @@ package com.kaviddiss.bootquartz;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({SchedulerConfig.class})
+@SpringApplicationConfiguration(classes = Application.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+
 }
--- src/main/java/com/kaviddiss/bootquartz/service/SampleService.java
+++ src/main/java/com/kaviddiss/bootquartz/service/SampleService.java
@@ -1,8 +1,13 @@
 package com.kaviddiss.bootquartz.service;
 
+import com.kaviddiss.bootquartz.job.SampleJob;
+import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by david on 2015-01-20.
@@ -10,9 +15,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class SampleService {
 
+    @Autowired
+    private Scheduler scheduler;
+
     private static final Logger LOG = LoggerFactory.getLogger(SampleService.class);
 
     public void hello() {
         LOG.info("Hello World!");
     }
+
+    @PostConstruct
+    public void test() throws SchedulerException {
+        JobDetail jobDetail = JobBuilder.newJob(SampleJob.class)
+                .storeDurably(true)
+                .build();
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .startNow()
+                .build();
+
+        scheduler.scheduleJob(jobDetail, trigger);
+    }
+
 }
